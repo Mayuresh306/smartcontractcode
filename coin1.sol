d0591c4206e26914798696c61d4e8f6b224f8866
Mayureshwar Mahadevan Reddy <mayureshreddy2006@gmail.com>
2025-03-30 15:54:54 +0000

    changes made in coin1.sol

--- coin1.sol
+++ coin1.sol
@@ -1,2 +1,11 @@
 // SPDX-License-Identifier-MIT
 pragma version ^0.8.0;
+
+contract coins {
+    string public Name;
+
+    function GetName(string memory _Name) public view returns(string memory) {
+        Name = _Name;
+        return Name;
+    } 
+}
